9c775031e09c3b2341c3b7163e1ecbb4db9a4c0e
jerrraa <jeromegould2002@gmail.com>
2023-04-05 13:35:03 -0700

    no code added however finished the layout for add and contacts.

--- MACos-project/AddViewController.swift
+++ MACos-project/AddViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    
+    @IBOutlet weak var FirstTextField: UITextField!
+    
+    @IBOutlet weak var LastTextField: UITextField!
+    
+    @IBOutlet weak var EmailTextField: UITextField!
+    
+    @IBOutlet weak var AddressTextField: UITextField!
+    
+    @IBOutlet weak var PhoneTextField: UITextField!
+
+    @IBOutlet weak var NoteTextView: UITextView!
+    
+    @IBOutlet weak var SubmitBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    	
 
 }
---
+++ MACos-project/DBmanager.swift
@@ -0,0 +1,16 @@
+//
+//  DBmanager.swift
+//  MACos-project
+//
+//  Created by user237152 on 4/5/23.
+//
+
+import Foundation
+
+import SQLite3
+
+class DBmanager {
+    
+    
+    
+}
